fe2ee67192db8fc003352f7e0f34e050b65fe614
Davide Pizzato <davide.pizzato@vimar.com>
2021-01-20 13:06:36 +0100

    day 1, puzzle 1

--- main.go
+++ main.go
@@ -1,9 +1,50 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
 )
 
 func main() {
-	fmt.Println("Hello, world.")
+	lines, err := readLines("input.txt")
+	assert(err)
+
+	for i, lhs := range lines {
+		for _, rhs := range lines[i+1:] {
+			if lhs+rhs == 2020 {
+				fmt.Println("answer: ", lhs*rhs)
+				return
+			}
+		}
+	}
+}
+
+func assert(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func readLines(fileName string) ([]int, error) {
+	file, err := os.Open(fileName)
+	if err != nil {
+		return nil, err
+	}
+
+	defer file.Close()
+
+	lines := []int{}
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		v, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			return nil, err
+		}
+		lines = append(lines, v)
+	}
+	err = scanner.Err()
+
+	return lines, err
 }
