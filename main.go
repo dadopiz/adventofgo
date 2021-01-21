d1151edff0c70849da6c0abd0d659f76d292f7c8
Davide Pizzato <davide.pizzato@vimar.com>
2021-01-21 17:18:26 +0100

    reworking for reuse

--- main.go
+++ main.go
@@ -11,9 +11,12 @@ func main() {
 	lines, err := readLines("input.txt")
 	assert(err)
 
-	for i, first := range lines {
-		for j, second := range lines[i+1:] {
-			for _, third := range lines[j+1:] {
+	values, err := convertoToIntArray(lines)
+	assert(err)
+
+	for i, first := range values {
+		for j, second := range values[i+1:] {
+			for _, third := range values[j+1:] {
 				if first+second+third == 2020 {
 					fmt.Println("answer: ", first*second*third)
 					return
@@ -29,7 +32,7 @@ func assert(err error) {
 	}
 }
 
-func readLines(fileName string) ([]int, error) {
+func readLines(fileName string) ([]string, error) {
 	file, err := os.Open(fileName)
 	if err != nil {
 		return nil, err
@@ -37,16 +40,26 @@ func readLines(fileName string) ([]int, error) {
 
 	defer file.Close()
 
-	lines := []int{}
+	lines := []string{}
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		v, err := strconv.Atoi(scanner.Text())
-		if err != nil {
-			return nil, err
-		}
-		lines = append(lines, v)
+		lines = append(lines, scanner.Text())
 	}
 	err = scanner.Err()
 
 	return lines, err
+}
+
+func convertoToIntArray(lines []string) ([]int, error) {
+	values := []int{}
+
+	for _, line := range lines {
+		v, err := strconv.Atoi(line)
+		if err != nil {
+			return nil, err
+		}
+		values = append(values, v)
+	}
+
+	return values, nil
 }
