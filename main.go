02a819ab08a2c9599ddbd4ed4473086eb5038fb8
Davide Pizzato <davide.pizzato@vimar.com>
2021-01-22 12:36:57 +0100

    day 3, puzzle 2

--- main.go
+++ main.go
@@ -6,9 +6,7 @@ import (
 )
 
 const (
-	tree  = '#'
-	right = 3
-	down  = 1
+	tree = '#'
 )
 
 type point struct {
@@ -16,9 +14,9 @@ type point struct {
 	y int
 }
 
-func (p *point) nextStep(size point) {
-	p.x += right
-	p.y += down
+func (p *point) nextStep(slope point, size point) {
+	p.x += slope.x
+	p.y += slope.y
 	if p.x >= size.x {
 		p.x = p.x - size.x
 	}
@@ -39,15 +37,30 @@ func main() {
 	treeMap, err := utils.ReadLines("input.txt")
 	utils.Assert(err)
 
+	slopes := []point{
+		{1, 1},
+		{3, 1},
+		{5, 1},
+		{7, 1},
+		{1, 2},
+	}
 	size := sizeOfTreeMap(treeMap)
-	position := point{}
-	treeCount := 0
-	for position.y < size.y {
-		if foundTree(treeMap, position) {
-			treeCount++
+	result := 1
+
+	for _, slope := range slopes {
+		treeCount := 0
+		position := point{}
+
+		for position.y < size.y {
+			if foundTree(treeMap, position) {
+				treeCount++
+			}
+			position.nextStep(slope, size)
 		}
-		position.nextStep(size)
+		fmt.Println("trees: ", treeCount)
+
+		result = result * treeCount
 	}
 
-	fmt.Println("trees: ", treeCount)
+	fmt.Println("result: ", result)
 }
