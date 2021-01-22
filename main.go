441bbdfdc665f1f38f724b13c84fda729e947bfb
Davide Pizzato <davide.pizzato@vimar.com>
2021-01-22 14:33:07 +0100

    day 4, puzzle 1

--- main.go
+++ main.go
@@ -3,64 +3,52 @@ package main
 import (
 	"adventofgo/utils"
 	"fmt"
+	"strings"
 )
-
-const (
-	tree = '#'
-)
-
-type point struct {
-	x int
-	y int
-}
-
-func (p *point) nextStep(slope point, size point) {
-	p.x += slope.x
-	p.y += slope.y
-	if p.x >= size.x {
-		p.x = p.x - size.x
-	}
-}
-
-func sizeOfTreeMap(treeMap []string) point {
-	return point{
-		x: len(treeMap[0]),
-		y: len(treeMap),
-	}
-}
-
-func foundTree(treeMap []string, p point) bool {
-	return []rune(treeMap[p.y])[p.x] == tree
-}
 
 func main() {
-	treeMap, err := utils.ReadLines("input.txt")
+	lines, err := utils.ReadLines("input.txt")
 	utils.Assert(err)
 
-	slopes := []point{
-		{1, 1},
-		{3, 1},
-		{5, 1},
-		{7, 1},
-		{1, 2},
-	}
-	size := sizeOfTreeMap(treeMap)
-	result := 1
-
-	for _, slope := range slopes {
-		treeCount := 0
-		position := point{}
-
-		for position.y < size.y {
-			if foundTree(treeMap, position) {
-				treeCount++
-			}
-			position.nextStep(slope, size)
+	datas := []string{}
+	data := ""
+	for _, line := range lines {
+		if line == "" {
+			datas = append(datas, data)
+			data = ""
+		} else {
+			data = data + line
 		}
-		fmt.Println("trees: ", treeCount)
+	}
+	datas = append(datas, data)
 
-		result = result * treeCount
+	count := 0
+	for _, data := range datas {
+		if requiredFields(data) {
+			count++
+		}
 	}
 
-	fmt.Println("result: ", result)
+	fmt.Println("count: ", count)
+}
+
+func requiredFields(data string) bool {
+	fields := []string{
+		"byr",
+		"iyr",
+		"eyr",
+		"hgt",
+		"hcl",
+		"ecl",
+		"pid",
+		//"cid",
+	}
+
+	for _, field := range fields {
+		if !strings.Contains(data, field) {
+			return false
+		}
+	}
+
+	return true
 }
