395c943b58311b7414a5eb3b073f671e6fa36d0e
Davide Pizzato <davide.pizzato@vimar.com>
2021-02-25 13:06:23 +0100

    day 5, puzzle 2

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"adventofgo/utils"
 	"fmt"
+	"sort"
 )
 
 const (
@@ -18,16 +19,23 @@ func main() {
 	lines, err := utils.ReadLines("input.txt")
 	utils.Assert(err)
 
-	maxSeatID := 0
+	seats := []int{}
 	for _, line := range lines {
-		row, column := seat(line)
-		seatID := seatID(row, column)
-		if seatID > maxSeatID {
-			maxSeatID = seatID
+		seats = append(seats, seatID(line))
+	}
+
+	sort.Ints(seats)
+	seat := seats[0]
+	for _, s := range seats {
+		if seat+1 == s {
+			seat = s
+		} else if seat != s {
+			seat = seat + 1
+			break
 		}
 	}
 
-	fmt.Println("seatID=", maxSeatID)
+	fmt.Println(seat)
 }
 
 func upperHalf(min int, max int) int {
@@ -38,11 +46,7 @@ func lowerHalf(min int, max int) int {
 	return (min + 1 + max) / 2
 }
 
-func seatID(row int, column int) int {
-	return row*columns + column
-}
-
-func seat(line string) (int, int) {
+func seatID(line string) int {
 	row := [2]int{0, rows - 1}
 	column := [2]int{0, columns - 1}
 	for _, c := range line {
@@ -57,5 +61,5 @@ func seat(line string) (int, int) {
 		}
 	}
 
-	return row[0], column[0]
+	return row[0]*columns + column[0]
 }
