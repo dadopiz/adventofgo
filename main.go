0fd396843f93e22055b83a8c99b1f8c25b22e019
Davide Pizzato <davide.pizzato@vimar.com>
2021-03-03 17:13:58 +0100

    day 6, puzzle 1

--- main.go
+++ main.go
@@ -3,63 +3,53 @@ package main
 import (
 	"adventofgo/utils"
 	"fmt"
-	"sort"
-)
-
-const (
-	rows    int  = 128
-	columns int  = 8
-	front   rune = 'F'
-	back    rune = 'B'
-	right   rune = 'R'
-	left    rune = 'L'
 )
 
 func main() {
 	lines, err := utils.ReadLines("input.txt")
 	utils.Assert(err)
 
-	seats := []int{}
+	groups := getGroups(lines)
+
+	res := 0
+	for _, g := range groups {
+		g = unique(g)
+		res += len(g)
+	}
+
+	fmt.Println("result:", res)
+}
+
+func getGroups(lines []string) []string {
+	groups := []string{}
+	group := ""
+
 	for _, line := range lines {
-		seats = append(seats, seatID(line))
-	}
-
-	sort.Ints(seats)
-	seat := seats[0]
-	for _, s := range seats {
-		if seat+1 == s {
-			seat = s
-		} else if seat != s {
-			seat = seat + 1
-			break
+		if line == "" {
+			groups = append(groups, group)
+			group = line
+		} else {
+			group += line
 		}
 	}
 
-	fmt.Println(seat)
+	if group != "" {
+		groups = append(groups, group)
+	}
+
+	return groups
 }
 
-func upperHalf(min int, max int) int {
-	return (min + max) / 2
-}
+func unique(group string) string {
+	keys := map[rune]bool{}
 
-func lowerHalf(min int, max int) int {
-	return (min + 1 + max) / 2
-}
-
-func seatID(line string) int {
-	row := [2]int{0, rows - 1}
-	column := [2]int{0, columns - 1}
-	for _, c := range line {
-		if c == front {
-			row[1] = upperHalf(row[0], row[1])
-		} else if c == back {
-			row[0] = lowerHalf(row[0], row[1])
-		} else if c == right {
-			column[0] = lowerHalf(column[0], column[1])
-		} else if c == left {
-			column[1] = upperHalf(column[0], column[1])
+	res := ""
+	for _, r := range group {
+		if _, value := keys[r]; !value {
+			keys[r] = true
+			res += string(r)
 		}
 	}
 
-	return row[0]*columns + column[0]
+	return res
 }
